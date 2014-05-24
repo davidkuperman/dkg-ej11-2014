7b7d3877f5ba45f80503b7464bf5bea0b2cb23bf
davidkuperman <kuper_7@hotmail.com>
2014-05-23 19:53:54 -0500

    styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Ej11::Application.routes.draw do
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  match '/about', to: 'static_pages#about', via: 'get'
+  match '/terms', to: 'static_pages#terms', via: 'get'
+  root 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
